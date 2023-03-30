65e26bf277224692757e13e62b1e19c12bfe5983
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-03-30 18:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+
+new Promise(function(resolve, reject) {
+
+setTimeout(() => resolve(1), 3000);
+
+}).then(function(result) {
+
+alert(result); // 1
+
+return new Promise((resolve, reject) => { // (*)
+setTimeout(() => resolve(result % 2 !== 0), 1000);
+});
+
+}).then(function(result) { // ()
+
+alert(result); // 2
+
+return new Promise((resolve, reject) => {
+setTimeout(() => resolve(result * 2), 1000);
+});
+
+}).then(function(result) {
+
+alert(result); // 4
+
+});
+const outputElement = document.getElementById('output');
+sayHello().then((message) => {
+outputElement.innerText = message;
